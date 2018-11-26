f9604f0826b70770c44e720faf380dadde78b6e5
adudzik <arek.dudzik@outlook.com>
2018-11-26 10:52:24 +0100

    Fixed problems with axis scale

--- src/components/functions/forms/MoreOptionsForm.js
+++ src/components/functions/forms/MoreOptionsForm.js
@@ -14,19 +14,20 @@ const customPanelStyle = {
 };
 
 const MoreOptionsForm = (props) => {
-    const defValue = props.defaultValue === 'log' ? 'log' : 'linear';
+    const defXValue = props.defaultX === 'log' ? 'log' : 'linear';
+    const defYValue = props.defaultY === 'log' ? 'log' : 'linear';
     return (
         <Form layout='horizontal'>
             <Collapse bordered={false}>
                 <Panel header={<h6>More plot options</h6>} key="1" style={customPanelStyle}>
                     <FormItem style={{ margin: 6 }} label={'X-Axis'} labelCol={{ span: 5 }}>
-                        <RadioGroup onChange={props.handleXChange} defaultValue={defValue}>
+                        <RadioGroup onChange={props.handleXChange} defaultValue={defXValue}>
                             <RadioButton value='linear'>Linear</RadioButton>
                             <RadioButton value='log'>Logarithmic</RadioButton>
                         </RadioGroup>
                     </FormItem>
                     <FormItem style={{ margin: 6 }} label={'Y-Axis'} labelCol={{ span: 5 }}>
-                        <RadioGroup onChange={props.handleYChange} defaultValue={defValue}>
+                        <RadioGroup onChange={props.handleYChange} defaultValue={defYValue}>
                             <RadioButton value='linear'>Linear</RadioButton>
                             <RadioButton value='log'>Logarithmic</RadioButton>
                         </RadioGroup>
