0e1ea50b54e0760d37690753f389700a1969d1bb
Leszek Grzanka <leszek.grzanka@gmail.com>
2019-03-16 20:57:57 +0100

    biological dose models

---
+++ src/functionsFromC/AT_biological_dose_proton.js
@@ -0,0 +1,98 @@
+export default function AT_biological_dose_proton(parameters) {
+    let at_proton_rbe_multi = Module.cwrap('AT_proton_RBE_multi', 'null', ['number', 'array', 'number', 'number', 'number', 'number', 'number', 'number', 'number']);
+    let at_dose_bortfeld_gy_multi = Module.cwrap('AT_dose_Bortfeld_Gy_multi', 'null', ['number', 'array', 'number', 'number', 'number', 'number', 'number', 'number']);
+    let at_dose_bortfeld_gy_single = Module.cwrap('AT_dose_Bortfeld_Gy_single', 'number', ['number', 'number', 'number', 'number', 'number', 'number']);
+
+    /*********************STANDARD PARAMETER*************************/
+    if(typeof parameters.n === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER n IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let n = parameters.n;
+
+    /*********************INPUT ARRAY********************************/
+    if(typeof parameters.z_cm === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER z_cm IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let z_cm = parameters.z_cm;
+    let z_cmData = new Float64Array(z_cm);
+    let z_cmDataBytesNumber = z_cmData.length * z_cmData.BYTES_PER_ELEMENT;
+    let z_cmDataPointer = Module._malloc(z_cmDataBytesNumber);
+    let z_cmHeap = new Uint8Array(Module.HEAPF64.buffer, z_cmDataPointer, z_cmDataBytesNumber);
+    z_cmHeap.set(new Uint8Array(z_cmData.buffer));
+
+    /*********************STANDARD PARAMETER*************************/
+    if(typeof parameters.entrance_dose_Gy === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER entrance_dose_Gy IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let entrance_dose_Gy = parameters.entrance_dose_Gy;
+
+    /*********************STANDARD PARAMETER*************************/
+    if(typeof parameters.E_MeV_u === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER E_MeV_u IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let E_MeV_u = parameters.E_MeV_u;
+
+    /*********************STANDARD PARAMETER*************************/
+    if(typeof parameters.sigma_E_MeV_u === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER sigma_E_MeV_u IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let sigma_E_MeV_u = parameters.sigma_E_MeV_u;
+
+    /*** default value of eps parameter ***/
+    let eps = -1;
+
+    /*********************STANDARD PARAMETER*************************/
+    if(typeof parameters.ref_alpha_beta_ratio === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER ref_alpha_beta_ratio IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let ref_alpha_beta_ratio = parameters.ref_alpha_beta_ratio;
+
+    /*********************STANDARD PARAMETER*************************/
+    if(typeof parameters.rbe_model_no === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER rbe_model_no IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let rbe_model_no = parameters.rbe_model_no;
+
+    /*********************OUTPUT ARRAY*******************************/
+    let rbeReturnData = new Float64Array(new Array(n));
+    let rbeReturnDataBytesNumber = rbeReturnData.length * rbeReturnData.BYTES_PER_ELEMENT;
+    let rbeReturnDataPointer = Module._malloc(rbeReturnDataBytesNumber);
+    let rbeReturnHeap = new Uint8Array(Module.HEAPF64.buffer, rbeReturnDataPointer, rbeReturnDataBytesNumber);
+
+    let dose_GyReturnData = new Float64Array(new Array(n));
+    let dose_GyReturnDataBytesNumber = dose_GyReturnData.length * dose_GyReturnData.BYTES_PER_ELEMENT;
+    let dose_GyReturnDataPointer = Module._malloc(dose_GyReturnDataBytesNumber);
+    let dose_GyReturnHeap = new Uint8Array(Module.HEAPF64.buffer, dose_GyReturnDataPointer, dose_GyReturnDataBytesNumber);
+
+    /*********************CALL FUNCTION******************************/
+
+    let entrance_dose_for_unit_fluence_Gy = at_dose_bortfeld_gy_single(0.0, 1.0, E_MeV_u, sigma_E_MeV_u, 1, eps);
+    let fluence_cm2 = entrance_dose_Gy / entrance_dose_for_unit_fluence_Gy;
+
+
+    // start with rbe table
+    at_proton_rbe_multi(n, z_cmHeap, entrance_dose_Gy, E_MeV_u, sigma_E_MeV_u, eps, ref_alpha_beta_ratio, rbe_model_no, rbeReturnHeap.byteOffset);
+    let resultFromArray = new Float64Array(rbeReturnHeap.buffer, rbeReturnHeap.byteOffset, rbeReturnData.length);
+
+    // calculate dose table
+    at_dose_bortfeld_gy_multi(n, z_cmHeap, E_MeV_u, fluence_cm2, sigma_E_MeV_u, 1, eps, dose_GyReturnHeap.byteOffset);
+    let doseFromArray = new Float64Array(dose_GyReturnHeap.buffer, dose_GyReturnHeap.byteOffset, dose_GyReturnData.length);
+
+    // multiply RBE with dose
+    for (var i = 0; i < resultFromArray.length; i++) {
+        resultFromArray[i] *= doseFromArray[i];
+    }
+
+    Module._free(z_cmHeap.byteOffset);
+    Module._free(rbeReturnHeap.byteOffset);
+    Module._free(dose_GyReturnHeap.byteOffset);
+
+    return [].slice.call(resultFromArray);
+}
