da8abfc6fd49b8bc588fff2f9ae0a5949feaee65
Leszek Grzanka <grzanka@agh.edu.pl>
2019-10-02 18:21:59 +0200

    first working version

--- src/functionsFromC/AT_inactivation_cross_section_Katz_um2.js
+++ src/functionsFromC/AT_inactivation_cross_section_Katz_um2.js
@@ -14,7 +14,6 @@ export default function AT_KatzModel_inactivation_cross_section_m2(parameters) {
         return "error";
     }
     let E_MeV_u = parameters.E_MeV_u;
-    console.log("E_MeV_u", E_MeV_u);
     let E_MeV_uData = new Float64Array(E_MeV_u);
     let E_MeV_uDataBytesNumber = E_MeV_uData.length * E_MeV_uData.BYTES_PER_ELEMENT;
     let E_MeV_uDataPointer = Module._malloc(E_MeV_uDataBytesNumber);
@@ -32,26 +31,53 @@ export default function AT_KatzModel_inactivation_cross_section_m2(parameters) {
     let material_no = 1; // water
 
     /*********************STANDARD PARAMETER*************************/
-    let rdd_model = 7; // Cucinotta extended
 
-    /*********************INPUT ARRAY********************************/
 
     // middle one -> a0
-    let rdd_parameters = [5e-11,1e-8,1e-10];
-    console.log("rdd_parameters", rdd_parameters);
+    if(typeof parameters.a0_um === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER a0_um IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let a0_um = parameters.a0_um;
+
+    if(typeof parameters.rdd_er_no === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER rdd_er_no IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let rdd_er_no = parameters.rdd_er_no;
+
+    let er_model = 0; // default
+    let rdd_model = 0; // default
+    let rdd_parameters = []; // default
+    if( rdd_er_no === 1 ){ // Katz + Waligorski
+        er_model = 3; // Waligorski
+        rdd_model = 6; // Katz extended
+        rdd_parameters = [1e-10,a0_um * 1e-6,1e-10];
+    } else { // Cucinotta + Tabata
+        er_model = 6; // Katz
+        rdd_model = 7; // Cucinotta extended
+        rdd_parameters = [5e-11,a0_um * 1e-6,1e-10];
+    }
+
     let rdd_parametersData = new Float64Array(rdd_parameters);
     let rdd_parametersDataBytesNumber = rdd_parametersData.length * rdd_parametersData.BYTES_PER_ELEMENT;
     let rdd_parametersDataPointer = Module._malloc(rdd_parametersDataBytesNumber);
     let rdd_parametersHeap = new Uint8Array(Module.HEAPF64.buffer, rdd_parametersDataPointer, rdd_parametersDataBytesNumber);
     rdd_parametersHeap.set(new Uint8Array(rdd_parametersData.buffer));
 
-    /*********************STANDARD PARAMETER*************************/
-    let er_model = 7; // Tabata
-
     /*********************INPUT ARRAY********************************/
-        /// unused , D0, c, m, unused
-    let gamma_parameters = [0,2,1,1,0];
-    console.log("gamma_parameters", gamma_parameters);
+
+    if(typeof parameters.m === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER n IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let m = parameters.m;
+    if(typeof parameters.D0_Gy === "undefined"){
+        alert("MESSAGE TO DEVELOPER: NO PARAMETER D0_Gy IN OBJECT PASSED TO THIS FUNCTIONS");
+        return "error";
+    }
+    let D0_Gy = parameters.D0_Gy;
+    let gamma_parameters = [0,D0_Gy,1,m,0]; /// unused , D0, c, m, unused
     let gamma_parametersData = new Float64Array(gamma_parameters);
     let gamma_parametersDataBytesNumber = gamma_parametersData.length * gamma_parametersData.BYTES_PER_ELEMENT;
     let gamma_parametersDataPointer = Module._malloc(gamma_parametersDataBytesNumber);
@@ -68,17 +94,13 @@ export default function AT_KatzModel_inactivation_cross_section_m2(parameters) {
     let inactivation_cross_section_m2ReturnHeap = new Uint8Array(Module.HEAPF64.buffer, inactivation_cross_section_m2ReturnDataPointer, inactivation_cross_section_m2ReturnDataBytesNumber);
 
     /*********************CALL FUNCTION******************************/
-    console.log("n", n);
-    console.log("E_MeV_uHeap", E_MeV_uHeap);
-    console.log("particle_no", particle_no);
-    console.log("material_no", material_no);
-    console.log("rdd_model", rdd_model);
-    console.log("rdd_parametersHeap", rdd_parametersHeap);
-    console.log("er_model", er_model);
-    console.log("gamma_parametersHeap", gamma_parametersHeap);
-    console.log("stop_power_source", stop_power_source);
     let result = at_katzmodel_inactivation_cross_section_m2(n, E_MeV_uHeap, particle_no, material_no, rdd_model, rdd_parametersHeap, er_model, gamma_parametersHeap, stop_power_source, inactivation_cross_section_m2ReturnHeap.byteOffset);
     let resultFromArray = new Float64Array(inactivation_cross_section_m2ReturnHeap.buffer, inactivation_cross_section_m2ReturnHeap.byteOffset, inactivation_cross_section_m2ReturnData.length);
+
+    // multiply RBE with dose
+    for (var i = 0; i < resultFromArray.length; i++) {
+        resultFromArray[i] *= 1e6 * 1e6; // m2 -> um2
+    }
 
     Module._free(E_MeV_uHeap.byteOffset);
     Module._free(rdd_parametersHeap.byteOffset);
